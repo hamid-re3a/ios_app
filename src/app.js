c37b2828f5a7b5be43a8f385675032cf99b37e98
Admin <admin@Admins-Mac.local>
2018-05-09 09:56:16 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View , Text } from 'react-native';
+import Application from "./components/Aplication";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Application/>
+        )
+    }
+}
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container , Header , View , Text , Left , Button , Right , Content , Form , Item , Icon , Input } from 'native-base';
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Header style={{ backgroundColor : '#34495e'}} androidStatusBarColor="#2c3e50" iosBarStyle="light-content">
+                    <Left>
+                        <Button transparent>
+                            <Text style={{ fontFamily : 'IRANSansMobile' , color: 'white'}}>عضویت</Text>
+                        </Button>
+                    </Left>
+                    <Right>
+                        <Text style={{ fontFamily : 'IRANSansMobile' , color: 'white'}}>صفحه ورود</Text>
+                    </Right>
+                </Header>
+                <Content>
+                    <Form>
+                        <Item rounded>
+                            <Input placeholder='ایمیل خود را وارد کنید'/>
+                            <Icon active name='md-mail' />
+                        </Item>
+                        <Item rounded>
+                            <Input placeholder='پسورد خود را وارد کنید'/>
+                            <Icon active name='md-key' />
+                        </Item>
+                    </Form>
+                </Content>
+            </Container>
+
+        )
+    }
+}
