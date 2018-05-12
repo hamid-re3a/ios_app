1600e1e0528035f188a402f19d608512d6a0458d
Admin <admin@Admins-Mac.local>
2018-05-12 11:06:07 +0300

    sth is different here

--- android/app/src/main/java/com/reservina/MainApplication.java
+++ android/app/src/main/java/com/reservina/MainApplication.java
@@ -3,6 +3,12 @@ package com.reservina;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.ocetnik.timer.BackgroundTimerPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +28,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new PickerPackage(),
+            new RNFetchBlobPackage(),
+            new BackgroundTimerPackage(),
+            new MapsPackage(),
+            new ReactNativePushNotificationPackage()
       );
     }
 
